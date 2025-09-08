4e16e96be8518cbce9a04d36d33a04c998fe80b2
Andrea Ghensi <aghensi@systra.com>
2025-09-08 16:03:16 +0200

    chore: add JSDoc typing

--- ifc_anywidget/index.js
+++ ifc_anywidget/index.js
@@ -4,6 +4,10 @@ import * as BUI from "https://esm.sh/@thatopen/ui@3.1.1";
 import * as BUIC from "https://esm.sh/@thatopen/ui-obc@3.1.3";
 
 
+/**
+ * @param {OBC.Components} components
+ * @returns {OBC.FragmentsManager}
+ */
 async function initFragments(components) {
   const fragments = components.get(OBC.FragmentsManager);
   const githubUrl = "https://thatopen.github.io/engine_fragment/resources/worker.mjs";
@@ -16,6 +20,11 @@ async function initFragments(components) {
   return fragments;
 }
 
+/**
+ * @param {OBC.Components} components
+ * @param {BUI.Viewport} viewport
+ * @returns {OBC.World}
+ */
 function setupWorld(components, viewport) {
   const worlds = components.get(OBC.Worlds);
   const world = worlds.create();
@@ -28,6 +37,10 @@ function setupWorld(components, viewport) {
   return world;
 }
 
+/**
+ * @param {OBC.Components} components
+ * @returns {OBC.IfcLoader}
+ */
 async function setupLoader(components) {
   const ifcLoader = components.get(OBC.IfcLoader);
   await ifcLoader.setup({
@@ -41,6 +54,11 @@ async function setupLoader(components) {
   return ifcLoader;
 }
 
+/**
+ * @param {OBC.Components} components
+ * @param {OBC.World} world
+ * @returns {OBCF.Highlighter}
+ */
 function setupHighlighter(components, world) {
   const highlighter = components.get(OBCF.Highlighter);
   highlighter.setup({world});
@@ -95,7 +113,6 @@ export default {
       model.save_changes();
     });
 
-
     // region Properties table
     const [propertiesTable, updatePropertiesTable] =
       BUIC.tables.itemsData({
@@ -134,7 +151,8 @@ export default {
       return BUI.html`
         <bim-panel-section label="Element Data">
           <div style="display: flex; gap: 0.5rem;">
-            <bim-button @click=${expandTable} label=${propertiesTable.expanded ? "Collapse" : "Expand"}></bim-button>
+            <bim-button @click=${expandTable} label=${propertiesTable.expanded ? "Collapse" : "Expand"}>
+            </bim-button>
           </div>
           <bim-text-input @input=${onTextInput} placeholder="Search Property" debounce="250"></bim-text-input>
           ${propertiesTable}
